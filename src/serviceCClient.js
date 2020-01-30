45e8f1d26875778f3649239941da0b5d598d6658
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2020-01-30 11:06:33 +0100

    Changing so we can use imagePath instead of nested image property

--- src/serviceCClient.js
+++ src/serviceCClient.js
@@ -7,10 +7,14 @@ const options = {
     json: true
 };
 
+function getImagePath(animal) {
+    return animal.imagePath || animal.image.path;
+}
+
 function toFeedingInstructions(animal, timeOfDay) {
     return {
       name: animal.name,
-      image: process.env.API_ENDPOINT + animal.image.path,
+      image: process.env.API_ENDPOINT + getImagePath(animal),
       food: animal.foodSchedule[timeOfDay]
     };
 }
